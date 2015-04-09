d0839dd5d1d51190b338c531231cc953ecc89800
Jeremy Fee <jmfee@usgs.gov>
2015-04-09 16:10:53 -0600

    Add HazardCurveGraphView example

---
+++ example/HazardCurveGraphView.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var HazardCurveGraphView = require('HazardCurveGraphView');
+
+
+var el = document.querySelector('#example'),
+    view;
+
+view = HazardCurveGraphView({
+  el: el
+});
