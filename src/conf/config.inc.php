c79d5f1cc573f494208848dfd622615231ed83a2
Eric Martinez <emartinez@usgs.gov>
2015-04-09 17:51:52 -0600

    Set config to create global $DB. Granting access to DB_USER.

--- src/conf/config.inc.php
+++ src/conf/config.inc.php
@@ -8,11 +8,11 @@ $APP_DIR = $CONFIG['APP_DIR'];
 $DATA_DIR = $CONFIG['DATA_DIR'];
 $MOUNT_PATH = $CONFIG['MOUNT_PATH'];
 
-/* Database stuff, if applicable.  * /
+
 // Using NO_DB guard allows pages that do not need database connection to use
 // this same configuration file without adding in database overhead.
 if (!(isset($NO_DB) && $NO_DB)) {
   $DB = new PDO($CONFIG['DB_DSN'], $CONFIG['DB_USER'], $CONFIG['DB_PASS']);
   $DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $DB->exec('SET SEARCH_PATH = ' . $CONFIG['DB_SCHEMA']);
 }
-/**/
