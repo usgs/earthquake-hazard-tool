89a3a039a90d85122900949a09f9522b77478a41
Jeremy Fee <jmfee@usgs.gov>
2015-04-14 11:03:29 -0600

    Update Graph/Line view to use updated base class

--- src/htdocs/js/HazardCurveGraphView.js
+++ src/htdocs/js/HazardCurveGraphView.js
@@ -7,11 +7,6 @@ var d3 = require('d3'),
     Util = require('util/Util');
 
 
-var DEFAULTS = {
-  curves: null
-};
-
-
 /**
  * Display a HazardCurve as a Graph
  *
@@ -23,24 +18,32 @@ var DEFAULTS = {
  *        all options are passed to D3GraphView.
  * @param options.curves {Array|Collection}
  *        curves to display.
+ * @param options.yLines {Array<Object>}
+ *        lines to show as annotations.
+ *        each line object should have these properties:
+ *          line.classes {Array<String>} classes to add to line.
+ *          line.label {String} label for line.
+ *          line.value {Number} y value for line.
  */
 var HazardCurveGraphView = function (options) {
   var _this,
       _initialize,
       // variables
       _curves,
-      _curvesEl,
-      _initialized,
-      _tooltip,
+      _line,
       _views,
       _x,
-      _xAxis,
-      _xAxisEl,
+      _xAxisLog,
+      _xExtent,
       _y,
-      _yAxis,
-      _yAxisEl,
+      _yAxisLog,
+      _yExtent,
+      _yLines,
       // methods
+      _getX,
+      _getY,
       _onAdd,
+      _onChange,
       _onDeselect,
       _onRemove,
       _onReset,
@@ -49,37 +52,46 @@ var HazardCurveGraphView = function (options) {
   _this = D3GraphView(options);
 
   _initialize = function (options) {
-    var plotArea = _this.plotArea;
-
-    _initialized = false;
+    var controls;
 
     // set defaults
-    options = Util.extend({}, DEFAULTS, options);
+    options = options || {};
     // parse options
     _curves = options.curves || Collection();
     _views = Collection();
-
+    _yLines = options.yLines || [
+      {
+        anchor: 'right',
+        classes: ['rate', 'rate-2p50'],
+        label: '2% in 50 years',
+        value: -Math.log(0.98) / 50
+      },
+      {
+        anchor: 'right',
+        classes: ['rate', 'rate-10p50'],
+        label: '10% in 50 years',
+        value: -Math.log(0.90) / 50
+      }
+    ];
+    _xExtent = null;
+    _yExtent = null;
     _this.el.classList.add('HazardCurveGraph');
 
-    _xAxis = d3.svg.axis()
-        .orient('bottom')
-        .ticks(8, function (x) {
-          return x;
-        });
-    _xAxisEl = plotArea.append('svg:g')
-        .attr('class', 'x axis')
-        .attr('transform', 'translate(0,' + _this.height + ')');
-    _yAxis = d3.svg.axis()
-        .orient('left');
-    _yAxisEl = plotArea.append('svg:g')
-        .attr('class', 'y axis');
-    _curvesEl = plotArea.append('svg:g')
-        .attr('class', 'curves');
-
-    _tooltip = plotArea.append('svg:g')
-        .attr('class', 'tooltip');
-
-    _this.setScales(d3.scale.log(), d3.scale.log());
+    // show controls to toggle axis scale
+    controls = _this.el.querySelector('.controls');
+    controls.innerHTML = '<form>' +
+        '<label for="xAxisLog">' +
+          '<input type="checkbox" id="xAxisLog" checked="checked"/>' +
+          'X-axis log' +
+        '</label>' +
+        '<label for="yAxisLog">' +
+          '<input type="checkbox" id="yAxisLog" checked="checked"/>' +
+          'Y-axis log' +
+        '</label>';
+    _xAxisLog = controls.querySelector('#xAxisLog');
+    _xAxisLog.addEventListener('change', _onChange);
+    _yAxisLog = controls.querySelector('#yAxisLog');
+    _yAxisLog.addEventListener('change', _onChange);
 
     _curves.on('add', _onAdd);
     _curves.on('deselect', _onDeselect);
@@ -88,7 +100,34 @@ var HazardCurveGraphView = function (options) {
     _curves.on('select', _onSelect);
     _this.curves = _curves;
 
-    _initialized = true;
+    _line = d3.svg.line();
+    _line.x(_getX);
+    _line.y(_getY);
+
+    _yLines.forEach(function (y) {
+      var el = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+      if (y.classes) {
+        y.classes.forEach(function (c) {
+          el.classList.add(c);
+        });
+      }
+      y.el = d3.select(el);
+      y.textEl = y.el.append('text')
+          .attr('text-anchor', y.anchor || 'left')
+          .attr('x', 0)
+          .attr('y', 0)
+          .attr('dy', '1em')
+          .text(y.label);
+      y.pathEl = y.el.append('path');
+    });
+  };
+
+  _getX = function (d) {
+    return _x(d[0]);
+  };
+
+  _getY = function (d) {
+    return _y(d[1]);
   };
 
   /**
@@ -102,27 +141,47 @@ var HazardCurveGraphView = function (options) {
    *        used for batch updates.
    */
   _onAdd = function (curve, dontrender) {
-    var el = _curvesEl.append('svg:g');
+    var el,
+        view;
 
-    var view = HazardCurveLineView({
+    el = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+    view = HazardCurveLineView({
       curve: curve,
       el: el,
-      tooltip: _tooltip,
-      xScale: _x,
-      yScale: _y
+      graph: _this
     });
-
-    // click on curve to trigger select
-    el.on('click', function () {
-      _curves.select(curve);
-    });
-
+    // add to collection
     view.id = curve.id;
     _views.add(view);
+
+    // click on curve to trigger select
+    view._onClick = function () {
+      _curves.select(curve);
+    };
+    el.addEventListener('click', view._onClick);
+
+    // augment destroy method
+    view.destroy = Util.compose(function () {
+      el.removeEventListener('click', view._onClick);
+      view._onClick = null;
+    }, view.destroy);
+
+    _xExtent = null;
+    _yExtent = null;
 
     if (!dontrender) {
       _this.render();
     }
+  };
+
+  /**
+   * Event handler for axis check boxes.
+   */
+  _onChange = function () {
+    _this.model.set({
+      xAxisScale: _xAxisLog.checked ? d3.scale.log() : d3.scale.linear(),
+      yAxisScale: _yAxisLog.checked ? d3.scale.log() : d3.scale.linear()
+    });
   };
 
   /**
@@ -132,7 +191,7 @@ var HazardCurveGraphView = function (options) {
    *        curve that is no longer selected.
    */
   _onDeselect = function (curve) {
-    _views.get(curve.id).el.node().classList.remove('selected');
+    _views.get(curve.id).el.classList.remove('selected');
   };
 
   /**
@@ -144,11 +203,10 @@ var HazardCurveGraphView = function (options) {
   _onRemove = function (curve) {
     var view = _views.get(curve.id);
     _views.remove(view);
-
-    // remove click handler
-    view.el.on('click', null);
-
     view.destroy();
+
+    _xExtent = null;
+    _yExtent = null;
   };
 
   /**
@@ -159,9 +217,10 @@ var HazardCurveGraphView = function (options) {
    */
   _onReset = function (data) {
     _views.data().forEach(function (v) {
-      v.el.on('click', null);
       v.destroy();
     });
+    _xExtent = null;
+    _yExtent = null;
     _views.reset([]);
     data.forEach(function (d) {
       _onAdd(d, true);
@@ -176,62 +235,93 @@ var HazardCurveGraphView = function (options) {
    *        curve that was selected.
    */
   _onSelect = function (curve) {
-    _views.get(curve.id).el.node().classList.add('selected');
-  };
-
-  /**
-   * Update graph scales.
-   *
-   * @param xScale {d3.scale}
-   *        scale for x axis.
-   * @param yScale {d3.scale}
-   *        scale for y axis.
-   */
-  _this.setScales = function (xScale, yScale) {
-    _this.xScale = xScale;
-    _this.xScale.range([0, _this.width]);
-
-    _this.yScale = yScale;
-    _this.yScale.range([_this.height, 0]);
-
-    if (_initialized) {
-      _this.render();
-    }
+    _views.get(curve.id).el.classList.add('selected');
   };
 
   /**
    * Unbind event listeners and free references.
    */
   _this.destroy = Util.compose(function () {
+    _xAxisLog.removeEventListener('change', _onChange);
+    _yAxisLog.removeEventListener('change', _onChange);
+
+    _curves.off('add', _onAdd);
+    _curves.off('deselect', _onDeselect);
+    _curves.off('remove', _onRemove);
+    _curves.off('reset', _onReset);
+    _curves.off('select', _onSelect);
     _curves = null;
   }, _this.destroy);
+
+  _this.getXExtent = function () {
+    if (_xExtent !== null) {
+      return _xExtent;
+    }
+    _xExtent = [];
+    _views.data().forEach(function (v) {
+      _xExtent = _xExtent.concat(v.xExtent);
+    });
+    _xExtent = d3.extent(_xExtent);
+    return _xExtent;
+  };
+
+  _this.getYExtent = function () {
+    if (_yExtent !== null) {
+      return _yExtent;
+    }
+    _yExtent = [];
+    _views.data().forEach(function (v) {
+      _yExtent = _yExtent.concat(v.yExtent);
+    });
+    _yExtent = d3.extent(_yExtent);
+    _yExtent[0] = Math.max(_yExtent[0], 1e-13);
+    return _yExtent;
+  };
 
   /**
    * Draw/update the graph.
    */
-  _this.render = function () {
-    var views = _views.data(),
-        xExtent = [],
-        yExtent = [];
-    // find data extent
-    views.forEach(function (v) {
-      xExtent = xExtent.concat(v.xExtent);
-      yExtent = yExtent.concat(v.yExtent);
-    });
-    xExtent = d3.extent(xExtent);
-    yExtent = d3.extent(yExtent);
-    yExtent[0] = Math.max(yExtent[0], 1e-13);
-    _this.xScale.domain(xExtent);
-    _this.yScale.domain(yExtent);
-    // update axes
-    _xAxis.scale(this.xScale);
-    _xAxisEl.call(_xAxis);
-    _yAxis.scale(this.yScale);
-    _yAxisEl.call(_yAxis);
+  _this.plot = function (el) {
+    var views = _views.data();
+
+    if (views.length === 0) {
+      return;
+    }
+
     // update curves
     views.forEach(function (v) {
-      v.setScales(_this.xScale, _this.yScale);
+      el.appendChild(v.el);
       v.render();
+    });
+
+    // make sure using current scale
+    _x = _this.model.get('xAxisScale');
+    _y = _this.model.get('yAxisScale');
+
+    _yLines.forEach(function (y) {
+      var x;
+
+      // getBBox doesn't work until attached
+      el.appendChild(y.el.node());
+
+      // plot line at top
+      y.pathEl.attr('d', _line([
+        [_xExtent[0], y.value],
+        [_xExtent[1], y.value]
+      ]));
+
+      if (y.anchor && y.anchor !== 'left') {
+        x = _x(_xExtent[1]);
+        if (y.anchor === 'middle') {
+          x = x / 2;
+        } else {
+          // right, position at right edge
+          x = x - y.textEl.node().getBBox().width;
+        }
+        y.textEl.attr('x', x);
+      }
+      // position text and line at y value
+      y.textEl.attr('y', _y(y.value));
     });
   };
 
--- src/htdocs/js/HazardCurveLineView.js
+++ src/htdocs/js/HazardCurveLineView.js
@@ -40,17 +40,17 @@ var HazardCurveLineView = function (options) {
       _initialize,
       // variables
       _curve,
+      _data,
       _el,
-      _initData,
+      _graph,
       _line,
       _lineFormat,
-      _tooltip,
       _x,
       _y,
       // methods
-      _formatTooltip,
       _getX,
       _getY,
+      _initData,
       _onMouseOut,
       _onMouseOver,
       _onPointOut,
@@ -59,22 +59,19 @@ var HazardCurveLineView = function (options) {
   _this = View(options);
 
   _initialize = function (options) {
-    _el = _this.el;
+    _el = d3.select(_this.el);
     _el.attr('class', 'HazardCurveLine');
 
     _curve = options.curve;
-    _lineFormat = options.lineFormat || d3.svg.line();
-    _tooltip = options.tooltip;
-    _x = options.xScale;
-    _y = options.yScale;
+    _graph = options.graph;
 
+    _lineFormat = options.lineFormat || d3.svg.line();
     _lineFormat.x(_getX);
     _lineFormat.y(_getY);
 
     _line = _el.append('svg:path')
         .attr('class', 'line')
-        .attr('clip-path', 'url(#clipToPlotArea)');
-
+        .attr('clip-path', 'url(#plotAreaClip)');
 
     _el.on('mouseout', _onMouseOut);
     _el.on('mouseover', _onMouseOver);
@@ -112,64 +109,9 @@ var HazardCurveLineView = function (options) {
         yvals = _curve.get('yvals')[0].yvals;
     xvals = xvals.map(__replaceZeros);
     yvals = yvals.map(__replaceZeros);
-    _this.data = d3.zip(xvals, yvals);
+    _data = d3.zip(xvals, yvals);
     _this.xExtent = d3.extent(xvals);
     _this.yExtent = d3.extent(yvals);
-  };
-
-  /**
-   * Format tooltip content.
-   *
-   * @param lines {Array<Object>}
-   *        lines to show in tooltip.
-   *        each line has the following keys:
-   *        line.label {String}
-   *        line.value {String}
-   */
-  _formatTooltip = function (lines) {
-    var content,
-        outline,
-        padding = 5,
-        width,
-        y;
-
-    content = _tooltip.append('g')
-        .attr('class', 'tooltip-content');
-
-    outline = content.append('rect')
-        .attr('class', 'tooltip-outline');
-
-    lines = lines.map(function (line) {
-      var text = content.append('text');
-      if (typeof line === 'string') {
-        text.text(line);
-      } else {
-        if (line.label) {
-          text.append('tspan')
-              .attr('class', 'label')
-              .text(line.label);
-        }
-        if (line.value) {
-          text.append('tspan')
-              .attr('class', 'value')
-              .text(line.value);
-        }
-      }
-      return text;
-    });
-
-    width = 0;
-    y = padding;
-    lines.forEach(function (line) {
-      var bbox = line.node().getBBox();
-      width = Math.max(width, bbox.width);
-      y += bbox.height;
-      line.attr('x', padding).attr('y', y);
-      y += padding;
-    });
-
-    outline.attr('width', width + 2 * padding)
-        .attr('height', y + padding);
   };
 
   /**
@@ -197,8 +139,7 @@ var HazardCurveLineView = function (options) {
     point.setAttribute('r', point.getAttribute('r') / 2);
 
     // clear previous tooltip
-    _tooltip.attr('transform', null)
-        .selectAll('*').remove();
+    _graph.showTooltip(null, null);
   };
 
   /**
@@ -206,17 +147,13 @@ var HazardCurveLineView = function (options) {
    */
   _onPointOver = function (coords) {
     var curve = _curve.get(),
-        x,
-        y,
-        bbox,
-        point,
-        tooltipBbox;
+        point;
 
     point = d3.event.target;
     point.classList.add('mouseover');
     point.setAttribute('r', point.getAttribute('r') * 2);
 
-    _formatTooltip([
+    _graph.showTooltip(coords, [
       curve.edition.display,
       {
         'label': curve.xlabel + ': ',
@@ -228,40 +165,6 @@ var HazardCurveLineView = function (options) {
             coords[1].toExponential())
       }
     ]);
-
-    // center of point
-    x = _x(coords[0]);
-    y = _y(coords[1]);
-    // box rendering inside
-    bbox = _el.node().getBBox();
-    // box being rendered
-    tooltipBbox = _tooltip.node().getBBox();
-    // keep tooltip in graph area
-    if (x + tooltipBbox.width > bbox.width) {
-      x = x - tooltipBbox.width - 10;
-    } else {
-      x = x + 10;
-    }
-    if (y + tooltipBbox.height > bbox.height) {
-      y = y - tooltipBbox.height - 10;
-    } else {
-      y = y + 10;
-    }
-    // set position
-    _tooltip.attr('transform', 'translate(' + x + ',' + y + ')');
-  };
-
-  /**
-   * Set the x and y scales used during rendering.
-   *
-   * @param xScale {d3.scale}
-   *        x axis scale.
-   * @param yScale {d3.scale}
-   *        y axis scale.
-   */
-  _this.setScales = function (xScale, yScale) {
-    _x = xScale;
-    _y = yScale;
   };
 
   /**
@@ -270,8 +173,12 @@ var HazardCurveLineView = function (options) {
   _this.render = function () {
     var points;
 
+    // update scales
+    _x = _graph.model.get('xAxisScale');
+    _y = _graph.model.get('yAxisScale');
+
     points = _el.selectAll('.point')
-        .data(_this.data);
+        .data(_data);
     points.enter()
         .append('svg:circle')
         .attr('class', 'point')
@@ -286,25 +193,29 @@ var HazardCurveLineView = function (options) {
         .on('mouseover', _onPointOver)
         .remove();
 
-    _line.attr('d', _lineFormat(_this.data));
+    _line.attr('d', _lineFormat(_data));
   };
 
   /**
    * Unbind events and free references.
    */
   _this.destroy = Util.compose(function () {
-    var el;
-
-    // trigger d3 cleanup
-    _this.data = [];
-    _this.render();
+    var points;
 
     _el.on('mouseout', null);
     _el.on('mouseover', null);
 
+    // cleanup events
+    points = _el.selectAll('.point')
+        .on('mouseout', null)
+        .on('mouseover', null);
+    _data = null;
+    _x = null;
+    _y = null;
+
     // remove container
-    el = _el.node();
-    Util.detach(el);
+    Util.detach(_el.node);
+    _el = null;
   }, _this.destroy);
 
 
