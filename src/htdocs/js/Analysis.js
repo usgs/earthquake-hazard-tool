5c98e59982adcb25527b834cf3fa4c617b9dc728
Eric Martinez <emartinez@usgs.gov>
2015-04-14 09:54:47 -0600

    Re-ordered parameters. Removed generic curve parameter. Will be replaced at runtime with actual models.

--- src/htdocs/js/Analysis.js
+++ src/htdocs/js/Analysis.js
@@ -6,15 +6,18 @@ var Model = require('mvc/Model'),
 var Analysis = function (options) {
   var _this;
 
+
   _this = Model(Util.extend({
-    latitude: null,
-    longitude: null,
     edition: null,
     region: null,
-    vs30: null,
-    curve: null,
-    imt: null
+
+    latitude: null,
+    longitude: null,
+
+    imt: null,
+    vs30: null
   }, options));
+
 
   options = null;
   return _this;
