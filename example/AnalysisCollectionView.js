e6984531d7832f8671327f07e846244c90d1072c
Hal Simpson <hasimpson@usgs.gov>
2015-04-15 16:13:48 -0600

    changed example

--- example/AnalysisCollectionView.js
+++ example/AnalysisCollectionView.js
@@ -13,18 +13,18 @@ Xhr.ajax({
     var analysisCollection = new AnalysisCollection(),
         analysis,
         index = Date.now();
-    // for (var i = 0, len = data.response.length; i < len; i++) {
-    //   analysis = new Analysis();
-    //   analysis.set({'id': index++,
-    //       'latitude': data.response[i].metadata.latitude,
-    //       'longitude': data.response[i].metadata.longitude,
-    //       'edition': Meta(data.response[i].metadata.edition),
-    //       'region': Region(data.response[i].metadata.region),
-    //       'imt': Meta(data.response[i].metadata.imt),
-    //       'vs30': Meta(data.response[i].metadata.site)
-    //       });
-    //   analysisCollection.add(analysis);
-    // }
+    for (var i = 0, len = data.response.length; i < len; i++) {
+      analysis = new Analysis();
+      analysis.set({'id': index++,
+          'latitude': data.response[i].metadata.latitude,
+          'longitude': data.response[i].metadata.longitude,
+          'edition': Meta(data.response[i].metadata.edition),
+          'region': Region(data.response[i].metadata.region),
+          'imt': Meta(data.response[i].metadata.imt),
+          'vs30': Meta(data.response[i].metadata.site)
+          });
+      analysisCollection.add(analysis);
+    }
     AnalysisCollectionView({
       el: document.querySelector('#example'),
       analysisCollection: analysisCollection
