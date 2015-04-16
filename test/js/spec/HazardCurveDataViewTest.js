347f76f507b59f1d0111ecb031be29a0bba5f849
ehunter-usgs <ehunter@usgs.gov>
2015-04-16 12:09:16 -0600

    Add Collection.select() test

--- test/js/spec/HazardCurveDataViewTest.js
+++ test/js/spec/HazardCurveDataViewTest.js
@@ -1,4 +1,4 @@
-/* global chai, describe, it, before */
+/* global chai, describe, it, beforeEach, afterEach */
 'use strict';
 
 var expect = chai.expect,
@@ -10,9 +10,18 @@ var hazardCurveDataViewElement = document.createElement('div'),
     hazardCurveDataView,
     curves;
 
+var _fireClickEvent = function (target) {
+  var clickEvent = document.createEvent('MouseEvents');
+  clickEvent.initMouseEvent('click', true, true, window, 1, 0, 0);
+  target.dispatchEvent(clickEvent);
+};
+
 describe('Unit tests for "HazardCurveDataView"', function () {
 
-  before(function (done) {
+  beforeEach(function (done) {
+
+    document.querySelector('body').appendChild(hazardCurveDataViewElement);
+
     Xhr.ajax({
       url: 'data.json',
       success: function (data) {
@@ -24,6 +33,10 @@ describe('Unit tests for "HazardCurveDataView"', function () {
         done();
       }
     });
+  });
+
+  afterEach(function () {
+    document.querySelector('body').removeChild(hazardCurveDataViewElement);
   });
 
   describe('Constructor', function () {
@@ -44,6 +57,19 @@ describe('Unit tests for "HazardCurveDataView"', function () {
       /* jshint -W030 */
       expect(table).not.to.be.null;
       /* jshint +W030 */
+    });
+  });
+
+  describe('Collection', function () {
+    it('adds selected class on select', function () {
+      var tableCell = hazardCurveDataViewElement.querySelector('td'),
+          curveId = tableCell.getAttribute('data-id');
+
+      _fireClickEvent(tableCell);
+
+      // Selected curve is the clicked curve
+      expect(curves.getSelected()).to.be.equal(curves.get(curveId));
+      expect(tableCell.classList.contains('selected')).to.be.equal(true);
     });
   });
 
