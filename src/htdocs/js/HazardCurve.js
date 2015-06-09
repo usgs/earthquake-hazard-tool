290c2612188787e6160fb964818880168a313491
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-06-09 08:43:05 -0600

    added getx, gety, and get bounds these all will call interpolateLogLog but for testing while waiting on that method to be merged i am useing the interpolate method. these methods look for the bound around a point and interpolate these values.

--- src/htdocs/js/HazardCurve.js
+++ src/htdocs/js/HazardCurve.js
@@ -1,13 +1,17 @@
 'use strict';
 
 var Model = require('mvc/Model'),
+    Util = require('util/Util'),
 
-    Util = require('util/Util');
+    HazardUtil = require('HazardCurve');
 
 var CURVE_ID = 0;
 
 var HazardCurve = function (params) {
-  var _this;
+  var _this,
+
+      _getBounds,
+      _hazardUtil;
 
   _this = Model(Util.extend({
     id: 'curve-' + CURVE_ID++,
@@ -15,6 +19,61 @@ var HazardCurve = function (params) {
     data: []
   }, params));
 
+  _hazardUtil = HazardUtil();
+
+  _this.getX = function (y) {
+    var data;
+
+    data = _this.get('data');
+
+    return _getBounds(data, y, 0);
+  };
+
+  _this.getY = function (x) {
+    var data;
+
+    data = _this.get('data');
+
+    return _getBounds(data, x, 1);
+
+  };
+
+  _getBounds = function (data, target, index) {
+    var lower,
+        upper,
+        x,
+        x0,
+        x1,
+        y0,
+        y1;
+
+    lower = data[0];
+    upper = data[data.length - 1];
+
+    data.forEach(function (item) {
+      if (item[index] < target && (item[index] > lower[index])) {
+        lower = item;
+      }
+      if (item[index] > target && (item[index] < upper[index])) {
+        upper = item;
+      }
+    });
+
+    x0 = lower[0][0];
+    y0 = lower[0][1];
+    x1 = upper[0][0];
+    y1 = upper[0][1];
+    x  = target;
+
+    if (index === 0) {
+      x0 = y0;
+      y0 = x0;
+      x1 = y1;
+      y1 = x1;
+    }
+
+    return _hazardUtil.interpolate(x0, y0, x1, y1, x);
+  };
 
   params = null;
   return _this;
