778b6cd784ac6b8c8e6dad7ab56ad87d33979694
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-06-11 13:16:20 -0600

    changed for each loop to for loop. Fixed if statments to work correctly

--- src/htdocs/js/HazardCurve.js
+++ src/htdocs/js/HazardCurve.js
@@ -23,55 +23,109 @@ var HazardCurve = function (params) {
     _hazardUtil = HazardUtil;
   };
 
+  /**
+   * Gets X value with a given Y value by interpolating the bounds
+   *
+   * @param variable {int}
+   *
+   * This method is a little confusing. Once the X and Y values of the bounds
+   * are found the X and Y values are switched. This solves for x in the
+   * interpolate so x0's value is switched with
+   * y0's value, etc.
+   */
   _this.getX = function (y) {
-    var data;
-
-    data = _this.get('data');
-
-    return _this.getBounds(data, y, 0);
-  };
-
-  _this.getY = function (x) {
-    var data;
-
-    data = _this.get('data');
-
-    return _this.getBounds(data, x, 1);
-  };
-
-  _this.getBounds = function (data, target, index) {
-    var lower,
-        upper,
-        x,
+    var data,
+        i,
+        left,
+        point,
+        pointX,
+        pointY,
+        right,
         x0,
         x1,
         y0,
         y1;
 
-    lower = data[0];
-    upper = data[data.length - 1];
+    data = _this.get('data');
+    left = null;
+    right = null;
 
-    data.forEach(function (item) {
-      if (item[index] < target && (item[index] > lower[index])) {
-        lower = item;
+    for (i = 0; i < data.length; i++) {
+      point = data[i];
+      pointX = point[0];
+      pointY = point[1];
+
+      if (y === pointY) {
+        return pointX;
+      } else {
+        if (y > pointY) {
+          if (left === null || pointY > left[1]) {
+            left = point;
+          }
+        }
+        if (y < pointY) {
+          if (right === null || pointY < right[1]) {
+            right = point;
+          }
+        }
       }
-      if (item[index] > target && (item[index] < upper[index])) {
-        upper = item;
-      }
-    });
-
-    x0 = lower[0][0];
-    y0 = lower[0][1];
-    x1 = upper[0][0];
-    y1 = upper[0][1];
-    x  = target;
-
-    if (index === 0) {
-      x0 = y0;
-      y0 = x0;
-      x1 = y1;
-      y1 = x1;
     }
+
+    x0 = left[0];
+    y0 = left[1];
+    x1 = right[0];
+    y1 = right[1];
+
+    return _hazardUtil.interpolateLogLog(y0, x0, y1, x1, y);
+  };
+
+  /**
+   * Gets Y value with a given X value by interpolating the bounds
+   *
+   * @param variable {int}
+   */
+  _this.getY = function (x) {
+    var data,
+        i,
+        left,
+        point,
+        pointX,
+        pointY,
+        right,
+        x0,
+        x1,
+        y0,
+        y1;
+
+    data = _this.get('data');
+    left = null;
+    right = null;
+
+    for (i = 0; i < data.length; i++) {
+      point = data[i];
+      pointX = point[0];
+      pointY = point[1];
+
+      if (x === pointX) {
+        return pointY;
+      } else {
+        if (x > pointX) {
+          if (left === null || pointX > left[0]) {
+            left = point;
+          }
+        }
+        if (x < pointX) {
+          if (right === null || pointX < right[0]) {
+            right = point;
+          }
+        }
+      }
+    }
+
+    x0 = left[0];
+    y0 = left[1];
+    x1 = right[0];
+    y1 = right[1];
 
     return _hazardUtil.interpolateLogLog(x0, y0, x1, y1, x);
   };
