c9b7b73487d8015311e085cc75e9c80787fc90e1
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-06-15 09:25:40 -0600

    fixed typo in name

---
+++ src/htdocs/js/TimeHorizonInputView.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var Collection = require('mvc/Collection'),
+    selectedCollectionView = require('mvc/SelectionCollectionView');
+
+
+
+var TimeHorizonImputView = function () {
+  var _this;
+
+  _this = selectedCollectionView(extend
+};
+
+module.exports = TimeHorizonImputView;
