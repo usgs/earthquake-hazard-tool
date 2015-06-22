7e7f08b232d5d8dd6892bf34404df40bfb8d90be
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-06-22 13:16:22 -0600

    changed how this works uses an array now

--- example/ContourTypeView.js
+++ example/ContourTypeView.js
@@ -7,26 +7,28 @@ var Analysis = require('Analysis'),
     metadata = require('etc/metadata');
 
 var analyses,
-    contourTypes;
+    contourTypes,
+    collection;
 
-analyses = Collection();
+analyses = [];
 contourTypes = [];
-
 
 metadata.parameters.contourType.values.map(function (contourType) {
   contourTypes.push(Meta(contourType));
 });
 
-analyses.add(Analysis({
+analyses.push(Analysis({
   contourType: contourTypes[0]
 }));
 
-analyses.add(Analysis({
+analyses.push(Analysis({
   contourType: contourTypes[1]
 }));
+collection = Collection(analyses);
+collection.select(analyses[1]);
 
 ContourTypeView({
   el: document.querySelector('.contourType'),
   contourType: Collection(contourTypes),
-  collection: analyses
+  collection: collection
 });
