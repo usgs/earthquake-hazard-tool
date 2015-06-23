1a8ac3269df0f95059445a5adaf3edeb89939be8
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-06-23 12:56:05 -0600

    created test page

--- test/js/spec/LocationInfoViewTest.js
+++ test/js/spec/LocationInfoViewTest.js
@@ -1,1 +1,16 @@
+/* global chai, describe, it */
 'use strict';
+
+var LocationInfoView = require('LocationInfoView');
+
+var expect = chai.expect;
+
+describe('LocationInfoView', function () {
+  describe('Constructor', function () {
+    it('Can be defined', function () {
+      /* jshint -W030 */
+      expect(LocationInfoView).not.to.be.undefined;
+      /* jshint +W030 */
+    });
+  });
+});
