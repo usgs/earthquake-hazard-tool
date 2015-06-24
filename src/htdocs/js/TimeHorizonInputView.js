4b2b0318255c600689f02ef1030c653483b367e4
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-06-24 12:01:18 -0600

    set up validation check

--- src/htdocs/js/TimeHorizonInputView.js
+++ src/htdocs/js/TimeHorizonInputView.js
@@ -1,12 +1,14 @@
 'use strict';
 
-var SelectedCollectionView = require('mvc/SelectedCollectionView'),
+var ModalView = require('mvc/ModalView'),
+    SelectedCollectionView = require('mvc/SelectedCollectionView'),
     Util = require('util/Util');
 
 var TimeHorizonInputView = function (params) {
   var _this,
       _initialize,
 
+      _modal,
       _timeHorizonInput,
       _updateTimeHorizon;
 
@@ -24,17 +26,43 @@ var TimeHorizonInputView = function (params) {
 
   // Updates timeHorizon on the model
   _updateTimeHorizon = function () {
+    var timeHorizonInputValue;
+
     if (_this.model) {
-      if (_timeHorizonInput.value === '') {
-        _this.model.set({
-          'timeHorizon': null
-        });
-      } else {
-        _this.model.set({
-          'timeHorizon': parseInt(_timeHorizonInput.value, 10)
-        });
+      if (_timeHorizonInput.value) {
+        timeHorizonInputValue = parseInt(_timeHorizonInput.value, 10);
+        if (timeHorizonInputValue >= 1 && timeHorizonInputValue <= 5000) {
+          _this.model.set({
+            'timeHorizon': timeHorizonInputValue
+          });
+        } else {
+         _this.show();
+        }
       }
     }
+  };
+
+  _this.hide = function () {
+    _modal.hide();
+  };
+
+  _this.show = function () {
+
+
+    _modal = ModalView(_this.el, {
+      title: 'Validation error',
+      message: 'Time Horizon value must be between 0 and 5,000',
+      buttons: [
+        {
+          callback: function () {
+            _modal.hide();
+          },
+          text: 'ok'
+        }
+      ]
+    });
+
+    //_modal.show();
   };
 
   _this.render = function () {
