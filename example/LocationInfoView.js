703a8c3b246d98e0e9cd859f0966dded652a6607
jdbrown-usgs <jdbrown-usgs@usgs.gov>
2015-06-25 15:23:19 -0600

    added analyses and collection

--- example/LocationInfoView.js
+++ example/LocationInfoView.js
@@ -1,13 +1,29 @@
 'use strict';
 
-var LocationInfoView = require('LocationInfoView'),
+var Analysis = require('Analysis'),
+    Collection = require('mvc/Collection'),
+    LocationInfoView = require('LocationInfoView'),
     Data = require('etc/data');
 
 var latitude = Data.response[0].metadata.latitude,
     longitude = Data.response[0].metadata.longitude;
 
+var analyses,
+    collection;
+
+analyses = [];
+
+analyses.push(Analysis({
+  latitude: latitude,
+  longitude: longitude
+}));
+
+collection = Collection(analyses);
+collection.select(analyses[0]);
+
 LocationInfoView({
   el: document.querySelector('.locationInfoView'),
   latitude: latitude,
-  longitude: longitude
+  longitude: longitude,
+  collection: collection
 });
