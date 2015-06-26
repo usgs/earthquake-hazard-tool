530ecb59eda79d0c9cc83c61172e851cc8c30917
Eric Martinez <emartinez@usgs.gov>
2015-06-25 18:03:38 -0600

    Added convenience methods to analysis to get the actual models.

--- src/htdocs/js/Analysis.js
+++ src/htdocs/js/Analysis.js
@@ -1,6 +1,9 @@
 'use strict';
 
-var Model = require('mvc/Model'),
+var DependencyFactory = require('DependencyFactory'),
+
+    Model = require('mvc/Model'),
+
     Util = require('util/Util');
 
 var SALT = 'analysis',
@@ -9,6 +12,8 @@ var SALT = 'analysis',
 var Analysis = function (options) {
   var _this,
       _initialize,
+
+      _dependencyFactory,
 
       _createUniqueId;
 
@@ -28,10 +33,13 @@ var Analysis = function (options) {
   }, options));
 
   _initialize = function (/*options*/) {
+    _dependencyFactory = DependencyFactory.getInstance();
+
     if (typeof _this.id === 'undefined' || _this.id === null) {
       _this.set({id: _createUniqueId()});
     }
   };
+
 
   _createUniqueId = function () {
     return SALT + '-' + (new Date()).getTime() + '-' + (SEQUENCE++);
@@ -39,8 +47,31 @@ var Analysis = function (options) {
 
 
   _this.destroy = Util.compose(_this.destroy, function () {
+    _dependencyFactory = null;
+
     _createUniqueId = null;
   });
+
+  _this.getEdition = function () {
+    return _dependencyFactory.getEdition(_this.get('edition'));
+  };
+
+  // Not really needed, but for consistency with other methods ...
+  _this.getLocation = function () {
+    return _this.get('location');
+  };
+
+  _this.getRegion = function () {
+    return _dependencyFactory.getRegion(_this.get('region'));
+  };
+
+  _this.getVs30 = function () {
+    return _dependencyFactory.getSiteClass(_this.get('vs30'));
+  };
+
+  _this.getSpectralPeriod = function () {
+    return _dependencyFactory.getSpectralPeriod(_this.get('imt'));
+  };
 
   _initialize(options);
   options = null;
