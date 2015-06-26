2a0818d0b937838e3636063bdd032f0a454b1bcc
Eric Martinez <emartinez@usgs.gov>
2015-06-25 18:08:33 -0600

    Added contains method.

--- src/htdocs/js/Region.js
+++ src/htdocs/js/Region.js
@@ -15,6 +15,28 @@ var Region = function (options) {
     gridspacing: null
   }, options));
 
+
+  _this.contains = function (location) {
+    var latitude,
+        longitude,
+        minlatitude,
+        minlongitude,
+        maxlatitude,
+        maxlongitude;
+
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    minlatitude = _this.get('minlatitude');
+    minlongitude = _this.get('minlongitude');
+    maxlatitude = _this.get('maxlatitude');
+    maxlongitude = _this.get('maxlongitude');
+
+    return (latitude >= minlatitude && latitude <= maxlatitude &&
+        longitude >= minlongitude && longitude <= maxlongitude);
+  };
+
+
   options = null;
   return _this;
 };
