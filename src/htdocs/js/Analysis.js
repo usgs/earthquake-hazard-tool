326229f65551de50d6798bda51485e40ac6dc693
Eric Martinez <emartinez@usgs.gov>
2015-06-29 11:04:06 -0600

    Setting curves attribute. Forwarding changes to that collection on as changes to parent model.

--- src/htdocs/js/Analysis.js
+++ src/htdocs/js/Analysis.js
@@ -2,6 +2,7 @@
 
 var DependencyFactory = require('DependencyFactory'),
 
+    Collection = require('mvc/Collection'),
     Model = require('mvc/Model'),
 
     Util = require('util/Util');
@@ -15,7 +16,11 @@ var Analysis = function (options) {
 
       _dependencyFactory,
 
-      _createUniqueId;
+      _bindCurvesListeners,
+      _createUniqueId,
+      _originalSet,
+      _onCurvesChanged,
+      _unbindCurveListeners;
 
 
   _this = Model(Util.extend({
@@ -28,8 +33,9 @@ var Analysis = function (options) {
     vs30: null,
 
     timeHorizon: null,
-    contourType: null
+    contourType: null,
 
+    curves: Collection([])
   }, options));
 
   _initialize = function (/*options*/) {
@@ -38,19 +44,65 @@ var Analysis = function (options) {
     if (typeof _this.id === 'undefined' || _this.id === null) {
       _this.set({id: _createUniqueId()});
     }
+
+    _bindCurvesListeners();
   };
 
+  _bindCurvesListeners = function () {
+    var curves;
+
+    curves = _this.get('curves');
+    if (curves) {
+      curves.on('add', _onCurvesChanged);
+      curves.on('remove', _onCurvesChanged);
+      curves.on('reset', _onCurvesChanged);
+    }
+  };
 
   _createUniqueId = function () {
     return SALT + '-' + (new Date()).getTime() + '-' + (SEQUENCE++);
   };
 
+  _onCurvesChanged = function () {
+    _this.trigger('change:curves');
+    _this.trigger('change');
+  };
+
+  _unbindCurveListeners = function () {
+    var curves;
+
+    curves = _this.get('curves');
+    if (curves) {
+      curves.off('add', _onCurvesChanged);
+      curves.off('remove', _onCurvesChanged);
+      curves.off('reset', _onCurvesChanged);
+    }
+  };
 
   _this.destroy = Util.compose(_this.destroy, function () {
+    _unbindCurveListeners();
+
     _dependencyFactory = null;
 
     _createUniqueId = null;
   });
+
+  _originalSet = _this.set;
+  _this.set = function (attributes, options) {
+    var changingCurves;
+
+    changingCurves = attributes.hasOwnProperty('curves');
+
+    if (changingCurves) {
+      _unbindCurveListeners();
+    }
+
+    _originalSet.call(_this, attributes, options);
+
+    if (changingCurves) {
+      _bindCurvesListeners();
+    }
+  };
 
   _this.getEdition = function () {
     return _dependencyFactory.getEdition(_this.get('edition'));
