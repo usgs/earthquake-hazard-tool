755cf3033db208de7c6b5c8f769aeab2137821bd
Eric Martinez <emartinez@usgs.gov>
2015-11-04 07:59:16 -0700

    Initial implementation of deagg response.

---
+++ src/htdocs/js/deagg/DeaggResponse.js
@@ -0,0 +1,86 @@
+'use strict';
+
+
+var Deaggregation = require('deagg/Deaggregation'),
+
+    Collection = require('mvc/Collection'),
+    Model = require('mvc/Model'),
+
+    Util = require('util/Util');
+
+
+// Default values to be used by constructor
+var _DEFAULTS = {
+  metadata: {
+    rlabel: 'Distance',
+    mlabel: 'Magnitude',
+    εlabel: 'Contribution',
+    εbins: []
+  },
+  data: []
+};
+
+var _DEAGG_ID = 0;
+
+
+/**
+ * Class: DeaggResponse
+ *
+ * @param params {Object}
+ *      Configuration options. See _DEFAULTS for more details.
+ */
+var DeaggResponse = function (params) {
+  var _this,
+      _initialize;
+
+
+  // Inherit from parent class
+  _this = Model();
+
+  /**
+   * @constructor
+   *
+   */
+  _initialize = function (params) {
+    var attributes,
+        deaggs,
+        metadata;
+
+    params = Util.extend({}, params,
+        {id: 'deagg-response-' + (_DEAGG_ID++)}, _DEFAULTS);
+
+    metadata = params.metadata;
+    deaggs = [];
+
+    attributes = {
+      imt: metadata.imt.value,
+      rlabel: metadata.rlabel,
+      mlabel: metadata.mlabel,
+      εlabel: metadata.εlabel,
+      εbins: metadata.εbins,
+      deaggregations: Collection(params.data.map(Deaggregation))
+    };
+
+    // Should not have listeners yet, but silent anyway to short-circuit check
+    _this.set(attributes, {silent: true});
+  };
+
+
+  _this.destroy = Util.compose(function () {
+    var deaggs;
+
+    deaggs = _this.get('deaggregations');
+    deaggs.destroy();
+
+    _initialize = null;
+    _this =  null;
+  }, _this.destroy);
+
+
+  _initialize(params);
+  params = null;
+  return _this;
+};
+
+
+module.exports = DeaggResponse;
