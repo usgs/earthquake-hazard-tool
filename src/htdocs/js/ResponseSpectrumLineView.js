686ae36247ae78221098e7ece658ed38788270ed
Eric Martinez <emartinez@usgs.gov>
2015-11-09 17:26:55 -0700

    Refactored a bit and added some helper methods.

--- src/htdocs/js/ResponseSpectrumLineView.js
+++ src/htdocs/js/ResponseSpectrumLineView.js
@@ -4,17 +4,47 @@ var D3LineView = require('d3/D3LineView'),
     Util = require('util/Util');
 
 
-var ResponseSpectrumLineView = function (options) {
-  var _this;
+var ResponseSpectrumLineView = function (params) {
+  var _this,
+      _initialize,
+
+      _getImt,
+      _getPointClasses,
+      _onPointClick;
+
 
   _this = D3LineView(Util.extend({
     data: [],
     legend: null,
     showLine: true,
-    showPoints: true
-  }, options));
+    showPoints: true,
+    imt: null
+  }, params));
 
-  _this.el.classList.add('ResponseSpectrumLineView');
+  _initialize = function (/*params*/) {
+    _this.el.classList.add('ResponseSpectrumLineView');
+  };
+
+
+  _getImt = function (data/*, index, scope*/) {
+    return data[2];
+  };
+
+  _getPointClasses = function (data/*, index, scope*/) {
+    var classes = ['point'];
+
+    if (_getImt(data) === _this.model.get('imt')) {
+      classes.push('selected');
+    }
+
+    return classes.join(' ');
+  };
+
+  _onPointClick = function (/*data, index*/) {
+    console.log('point clicked!');
+    console.log(arguments);
+  };
+
 
   /**
    * Override y value formatting.
