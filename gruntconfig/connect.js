80db14fb8ed070429cdc4edf43fe8adec72cb661
Eric Martinez <emartinez@usgs.gov>
2015-11-17 07:25:42 -0700

    Added middleware to template mount so examples can be viewed.

--- gruntconfig/connect.js
+++ gruntconfig/connect.js
@@ -127,6 +127,7 @@ var connect = {
   template: {
     options: {
       base: ['node_modules/hazdev-template/dist/htdocs'],
+      middleware: addMiddleware,
       port: config.templatePort
     }
   }
