4204a46fca9ad2285cbe455c7991a68b07736f13
emartinez-usgs <emartinez@usgs.gov>
2017-01-25 15:18:13 -0700

    Added beta label. Added issue tracker link.

--- src/htdocs/_navigation.inc.php
+++ src/htdocs/_navigation.inc.php
@@ -2,4 +2,8 @@
 
 include_once '../conf/config.inc.php';
 
-print navItem($MOUNT_PATH . '/index.php', 'Hazard Tool');
+print navGroup(
+  navItem($MOUNT_PATH . '/index.php', 'Hazard Tool'),
+  navItem('https://github.com/usgs/earthquake-hazard-tool/issues',
+      'Issue Tracker')
+);
